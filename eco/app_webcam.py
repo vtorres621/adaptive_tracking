7ad033ef11a5566f11c291d95cf2293fa82111fd
Victor Torres <v.torres621@gmail.com>
2021-10-26 12:06:01 -0700

    added webcam and timing scripts

--- eco/app_webcam.py
+++ eco/app_webcam.py
@@ -1,5 +1,6 @@
 from ctypes import *
 import numpy as np
+import cv2
 import vart
 import os
 import xir
@@ -11,13 +12,13 @@ from src.datasets import get_dataset
 import importlib
 from src.kalman_filter import KFilter, Box
 from src.dpu_utils import get_child_subgraph_dpu, runDPU
-from src.pytracking_utils import Sequence, _read_image, preprocess, initial_preprocess, get_parameters
-
+from src.pytracking_utils_w import Sequence, _read_image, preprocess, initial_preprocess, get_parameters
+from src.yolo_utils import runYoloDPU
 
 divider='---------------------------'
 
 
-def app(image_dir,threads,model, seq: Sequence, key_frames = 5):
+def app(image_dir,threads,eco_model, seq: Sequence, key_frames = 5):
     
     print(divider)
 
@@ -25,12 +26,28 @@ def app(image_dir,threads,model, seq: Sequence, key_frames = 5):
     listimage=os.listdir(image_dir)
     listimage.sort()
 
-    # Deserialize subgraph
-    g = xir.Graph.deserialize(model)
+    # Create ECO DPU runner
+    g = xir.Graph.deserialize(eco_model)
     subgraphs = get_child_subgraph_dpu(g)
+    ecoRunner = vart.Runner.create_runner(subgraphs[0], "run")
 
-    # Creating DPU runners
-    dpuRunner = vart.Runner.create_runner(subgraphs[0], "run")
+    # Create YOLO DPU runner
+    yolo_model = 'yolov3.xmodel'
+    y = xir.Graph.deserialize(yolo_model)
+    yoloSubgraphs = get_child_subgraph_dpu(y)
+    yoloRunner = vart.Runner.create_runner(yoloSubgraphs[0], "run")
+
+
+
+    # Initialize Camera
+    ramp_frames = 10
+    camera = cv2.VideoCapture(0)
+    camera.open(0, cv2.CAP_V4L2)
+    camera.set(3, 640)
+    camera.set(4, 480)
+
+    for i in range(ramp_frames):
+        _, temp = camera.read()
 
     # Run loop
     print("[INFO][app.py] Running loop: ")
@@ -40,21 +57,42 @@ def app(image_dir,threads,model, seq: Sequence, key_frames = 5):
         # First frame: Initialize ECO, Create Kalman Filter
         if frame_num == 0: 
             #Read image
-            image = _read_image(os.path.join(image_dir,listimage[frame_num]))
+            #image = _read_image(os.path.join(image_dir,listimage[frame_num]))
+            _, image = camera.read()
+            #cv2.imshow("Image", image)
+            #cv2.waitKey(0)
+            #cv2.destroyAllWindows()
 
-            #Get initial info of sequence
+            
+            #Get initial bounding box using yolo
+            initial_bbox = runYoloDPU(yoloRunner, image)
+
+            #Limit initial bounding box size
+            x, y, w, h = initial_bbox
+
+            mid_x = (x + w/2)
+            mid_y = (y + h/2)
+            g_w = 65
+            g_h = 30
+            g_x = mid_x - g_w / 2
+            g_y = mid_y - g_h / 2
+            initial_bbox = [g_x, g_y, g_w, g_h]
+
+            #Get initial info
             init_info = seq.init_info()
             info = seq.init_info()
+            info['init_bbox'] = initial_bbox
+            init_info['init_bbox'] = initial_bbox
 
             #Get patches
-            _, im_patches, _, sample_pos, _, img_sample_sz, _ = initial_preprocess(image, info)           
+            _, im_patches, _, sample_pos, _, img_sample_sz, _ = initial_preprocess(image, initial_bbox)        
         
             #Get previous output
-            out = {'target_bbox': info.get('init_bbox')}
+            out = {'target_bbox': initial_bbox}
             info['previous_output'] = out
             
             #Run DPU
-            features = runDPU(dpuRunner, im_patches)
+            features = runDPU(ecoRunner, im_patches)
             
             # Create parameters object with FPGA produced features
             params = get_parameters(features)
@@ -89,10 +127,18 @@ def app(image_dir,threads,model, seq: Sequence, key_frames = 5):
             bbox = [x,y,w,h]
             bboxes.append(bbox)
 
+            #Save frame with bbox
+            image = cv2.rectangle(image, (bbox[0],bbox[1]), (bbox[0]+bbox[2],bbox[1]+bbox[3]), (0,255,0), 1)
+            frame_name = 'results/'+str(frame_num+1).zfill(4)+'.png'
+            cv2.imwrite(frame_name, image)
+
+
         # Key frame: Update Kalman Filter
         elif frame_num>0 and frame_num%key_frames==0:
             #Read image
-            image = _read_image(os.path.join(image_dir,listimage[frame_num]))
+            #image = _read_image(os.path.join(image_dir,listimage[frame_num]))
+            _, image = camera.read()
+
 
             #Get info
             info = seq.frame_info(frame_num)
@@ -104,7 +150,7 @@ def app(image_dir,threads,model, seq: Sequence, key_frames = 5):
             info['previous_output'] = prev_output
             
             #Run DPU
-            features = runDPU(dpuRunner, im_patches)
+            features = runDPU(ecoRunner, im_patches)
             
             #Run ECO Track
             out, sample_pos, sample_scales = tracker.track(features, image, info)
@@ -127,6 +173,12 @@ def app(image_dir,threads,model, seq: Sequence, key_frames = 5):
             #Append and print bouding box
             bbox = [x,y,w,h]
             bboxes.append(bbox)
+            
+            #Save frame with bbox
+            image = cv2.rectangle(image, (bbox[0],bbox[1]), (bbox[0]+bbox[2],bbox[1]+bbox[3]), (0,255,0), 1)
+            frame_name = 'results/'+str(frame_num+1).zfill(4)+'.png'
+            cv2.imwrite(frame_name, image)
+
             print(bbox)
 
         
@@ -141,6 +193,12 @@ def app(image_dir,threads,model, seq: Sequence, key_frames = 5):
             #Pass bounding box to next frame
             out = {'target_bbox': [kf_x,kf_y,kf_w,kf_h]}
             prev_output = OrderedDict(out)
+
+            #Read and write image
+            _, image = camera.read()
+            image = cv2.rectangle(image, (kf_x,kf_y), (kf_x+kf_w,kf_y+kf_h), (0,255,0), 1)
+            frame_name = 'results/'+str(frame_num+1).zfill(4)+'.png'
+            cv2.imwrite(frame_name, image)
 
             #Append and print bouding box
             bbox = [kf_x,kf_y,kf_w,kf_h]
